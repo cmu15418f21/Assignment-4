e06eca935476added2f75f3455c24b094a88f741
joseph-x-li <13826851+joseph-x-li@users.noreply.github.com>
2021-10-14 01:38:36 -0400

    initial commit

---
+++ code/wireroute.cpp
@@ -0,0 +1,14 @@
+#include "wireroute.h"
+#include "mpi.h"
+#include <assert.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <libgen.h>
+
+// Perform computation, including reading/writing output files
+void compute(int procID, int nproc, char *inputFilename, double prob, int numIterations) {
+  // TODO Implement code here
+  // TODO Decide which processors should be reading/writing files
+}
+
